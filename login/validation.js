b284c0db2050905a479eb57e2edf9760afa50210
Mouhcine Daali <daalim277@gmail.com>
2022-01-13 16:29:25 +0100

    add validation checker script

---
+++ login/validation.js
@@ -0,0 +1,53 @@
+const form = document.querySelector("#form"),
+        email = document.querySelector("#email"),
+        password = document.querySelector("#password");
+
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    
+    verifyInput();
+});
+
+const verifyInput = () => {
+    const emailValue = email.value.trim(),
+            passwordValue = password.value.trim();
+
+    if (emailValue === "") {
+        errorMessage(email, "Email cannot be blank");
+    } else if (!validateEmail(emailValue)) {
+        errorMessage(email, "Not a valid email");
+    } else {
+        successMessage(email);
+    }
+
+    if (passwordValue === "") {
+        errorMessage(password, "Password cannot be blank");
+    } else {
+        successMessage(password);
+    }
+
+}
+
+const errorMessage = (input, message) => {
+    const   inputContainer = input.parentElement;
+    const   p = inputContainer.querySelector("p");
+
+    //add success class
+    inputContainer.className = "input-container error";
+    p.innerText = message;
+}
+
+const successMessage = input => {
+    const inputContainer = input.parentElement;
+
+    //add success class
+    inputContainer.className = "input-container success";
+}
+
+// const isValidEmail = email => {
+// 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+// }
+const validateEmail = (email) => {
+    return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  };
